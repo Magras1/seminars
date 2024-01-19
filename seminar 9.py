67746e4b5ea1d4469f4b79151780584258fa380e
Magras1 <145995041+Magras1@users.noreply.github.com>
2024-01-19 06:46:51 +0300

    Update seminar 9.py

--- seminar 9.py
+++ seminar 9.py
@@ -15,5 +15,71 @@ list_1 = [i * k for i, k in enumerate([2, 4, -2, -3, 0 , 11 , 3 -1])]
 print(list_1)
 
 4.18)
-
 print([i if i >= 0 else '' for i in [2, 4, -2, -3, 0 , 11 , 3 -1]])
+
+4.19)
+print([[2, 4, -2, -3, 0 , 11 , 3 -1][i] if [2, 4, -2, -3, 0 , 11 , 3 -1][i] >= 0 else i for i in range(len([2, 4, -2, -3, 0 , 11 , 3 -1]))])
+
+5.1)  
+def pr(a: int or float, b: int or float) -> int or float:
+    return a * b
+print(pr(int(input('a = ')), int(input('b = '))))
+
+5.2)
+def pr(a: int or float, b: int or float = 1, c: int or float = 1) -> int or float:
+    return a * b * c
+print(pr(2))
+print(pr(2, 5))
+print(pr(2, 3, 54))
+
+5.3)
+print(pr(*(15, 10 ,5)))
+print(pr(*(3, 1)))
+print(pr(*[2, 35, 55]))
+print(pr(*(5, 10 ,15, 20)[:3]))
+print(pr(*(5, 10 ,15, 20)[1:4]))
+
+5.4)
+def pr(*a) -> int or float:
+    k = 1
+    for i in a:
+        k *= i
+    return k
+print(pr(*(15, 10 ,5)))
+print(pr(*(3, 1)))
+print(pr(*[2, 35, 55]))
+print(pr(*(5, 10 ,15, 20)[:3]))
+print(pr(*(5, 10 ,15, 20)[1:4]))
+
+5.5)
+def pro(k):
+    if k == '*':
+        return 10 * 5  
+    elif k == '-':
+        return 10 - 5
+    elif k == '+':
+        return 10 + 5
+    elif k == '/':
+        return 10 // 5
+print(pro(input()))
+
+5.6)
+def calc(op):
+    if op.find('-') != -1:
+        return int(op.split(' - ')[0]) - int(op.split(' - ')[1])
+    elif op.find('*') != -1:
+        return int(op.split(' * ')[0]) * int(op.split(' * ')[1])
+    elif op.find('+') != -1:
+        return int(op.split(' + ')[0]) + int(op.split(' + ')[1])
+    elif op.find('/') != -1:
+        return int(op.split(' / ')[0]) // int(op.split(' / ')[1])
+    elif op.find('%') != -1:
+        return int(op.split(' % ')[0]) % int(op.split(' % ')[1])
+    elif op.find('^') != -1:
+        return int(op.split(' ^ ')[0]) ** int(op.split(' ^ ')[1])
+print(calc(input()))
+
+
+    
+
+
